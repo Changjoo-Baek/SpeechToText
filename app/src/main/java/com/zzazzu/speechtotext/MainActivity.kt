f6be18ba5d7518c87f482638f8cb052f7416d358
bucephalus41 <bucephalus41@gmail.com>
2021-07-04 20:21:02 +0900

    입력 완료 모두 끝

--- app/src/main/java/com/zzazzu/speechtotext/MainActivity.kt
+++ app/src/main/java/com/zzazzu/speechtotext/MainActivity.kt
@@ -1,11 +1,84 @@
 package com.zzazzu.speechtotext
 
+import android.Manifest
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var speechRecognizer: SpeechRecognizer? = null
+    private val REQUEST_CODE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // permission 확인
+        if (Build.VERSION.SDK_INT >=23)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.INTERNET,Manifest.permission.RECORD_AUDIO), REQUEST_CODE)
+
+
+        // STTButton 클릭시, 음성인식 기능 실행
+        STTButton.setOnClickListener {startSTT()}
+
     }
+
+    private fun startSTT() {
+        val speechRecognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+            putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, packageName)
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+        }
+
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this).apply {
+            setRecognitionListener(recognitionListener())
+            startListening(speechRecognizerIntent)
+        }
+
+    }
+
+    private fun recognitionListener() = object : RecognitionListener {
+
+        override fun onReadyForSpeech(params: Bundle?) = Toast.makeText(this@MainActivity, "음성인식 시작", Toast.LENGTH_SHORT).show()
+
+        override fun onRmsChanged(rmsdB: Float) {}
+
+        override fun onBufferReceived(buffer: ByteArray?) {}
+
+        override fun onPartialResults(partialResults: Bundle?) {}
+
+        override fun onEvent(eventType: Int, params: Bundle?) {}
+
+        override fun onBeginningOfSpeech() {}
+
+        override fun onEndOfSpeech() {}
+
+        override fun onError(error: Int) {
+            when(error) {
+                SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS -> Toast.makeText(
+                    this@MainActivity,
+                    "퍼미션 없음",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        override fun onResults(results: Bundle) {
+            Toast.makeText(this@MainActivity, "음성인식 종료", Toast.LENGTH_SHORT).show()
+            textView.text = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)!![0]
+        }
+
+
+    }
+
+
+
 }
